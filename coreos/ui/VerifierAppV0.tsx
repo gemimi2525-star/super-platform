4afd45e452e79ffb455146dd6e6477d055eb08f6
Jukkrit <gemimi2525@gmail.com>
2026-02-05 21:39:21 +0700

    fix(build): add use client to VerifierAppV0 and verify casing

--- coreos/ui/VerifierAppV0.tsx
+++ coreos/ui/VerifierAppV0.tsx
@@ -1,4 +1,6 @@
 
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { useFileSystem } from '../../lib/filesystem/FileSystemProvider';
 import { FileSystemError, FsError } from '../../lib/filesystem/types';
