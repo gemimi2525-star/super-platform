3dfeae4c09c04ea3f0e712731db97ee5f42d0687
Jukkrit <gemimi2525@gmail.com>
2026-02-05 21:50:58 +0700

    fix(build): add root layout for app/core

---
+++ app/core/layout.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+export default function CoreLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
